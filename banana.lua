6c2e2a93a35f6f8809c665ca5a09ab134b0f8330
wraithkings <wraithkings@gmail.com>
2010-02-17 17:56:02 +0800

    refactory to calculator example

--- banana.lua
+++ banana.lua
@@ -1,15 +1,22 @@
 
+local oldpath = package.path
 package.path = '?.luac;?.lua;./lua/?.luac;./lua/?.lua'
 require 'metalua.mlc_xcall'
 require 'metalua.compiler'
 local gwt_define = './lua/gwt_define.mlua'
 mlc.function_of_luacstring(mlc.luacstring_of_ast(mlc.luafile_to_ast(gwt_define)))()
+package.path = oldpath
 
-local filename = './test/simple_gwt.feature'
+local filename = './test/example/step_definitons/calculator_steps.lua'
 local ast = mlc.luafile_to_ast(filename)
 local bytecode = mlc.luacstring_of_ast(ast)
 local f = mlc.function_of_luacstring(bytecode)
+f()
 print("run ".. filename)
 print("")
-f()
-print("Bye")
+_Given:fun("I have entered 6 into the calculator")
+_Given:fun("I have entered 7 into the calculator")
+_When:fun("I press add")
+_Then:fun("the result should be 13 on the screen")
+
+print("Test OK!")
--- test/example/step_definitons/calculator_steps.lua
+++ test/example/step_definitons/calculator_steps.lua
@@ -1,9 +1,41 @@
 
-Given [[I have entered {d+} into the calculator]] do |x|
+local calculator = {}
+calculator.enter = function(self, d)
+    if not self._nums then
+        self._nums = {}
+    end
+    
+    table.insert(self._nums, d)
 end
 
-When [[I press {w+}]] do |x|
+calculator.add = function(self)
+    if not self.result then
+        self.result = 0
+    end
+    
+    if not self._nums then
+        self.result = 0
+        return
+    elseif #self._nums == 0 then
+        self.result = 0
+        return
+    elseif #self._nums == 1 then
+        self.result = self._nums[1]
+    end
+    
+    for i, v in ipairs(self._nums) do
+        self.result = self.result + self._nums[i]
+    end
 end
 
-Then [[the result should be {d+} on the screen]] do |x|
+Given [[I have entered (%d+) into the calculator]] do |x|
+    calculator:enter(tonumber(x))
+end
+
+When [[I press (%w+)]] do |x|
+    calculator[x](calculator)
+end
+
+Then [[the result should be (%d+) on the screen]] do |x|
+    assert(calculator.result == tonumber(x), calculator.result)
 end

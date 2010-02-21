7d756bc224e9c581e2cef2b0e4edb6ac221ccc4d
wraithkings <wraithkings@gmail.com>
2010-02-22 00:13:32 +0800

    refactory

--- banana.lua
+++ banana.lua
@@ -4,19 +4,32 @@ package.path = '?.luac;?.lua;./lua/?.luac;./lua/?.lua'
 require 'metalua.mlc_xcall'
 require 'metalua.compiler'
 local gwt_define = './lua/gwt_define.mlua'
-mlc.function_of_luacstring(mlc.luacstring_of_ast(mlc.luafile_to_ast(gwt_define)))()
+dofile(gwt_define)
 package.path = oldpath
 
 local filename = './test/example/step_definitons/calculator_steps.lua'
-local ast = mlc.luafile_to_ast(filename)
-local bytecode = mlc.luacstring_of_ast(ast)
-local f = mlc.function_of_luacstring(bytecode)
-f()
+dofile(filename)
 print("run ".. filename)
 print("")
-_Given:fun("I have entered 6 into the calculator")
-_Given:fun("I have entered 7 into the calculator")
-_When:fun("I press add")
-_Then:fun("the result should be 13 on the screen")
+local scenario = {}
+scenario.Given = {}
+scenario.When = {}
+scenario.Then = {}
+table.insert(scenario.Given, "I have entered 6 into the calculator")
+table.insert(scenario.Given, "I have entered 7 into the calculator")
+table.insert(scenario.When, "I press add")
+table.insert(scenario.Then, "the result should be 13 on the screen")
+
+for i, v in ipairs(scenario.Given) do
+    _Given:fun(scenario.Given[i])
+end
+
+for i, v in ipairs(scenario.When) do
+    _When:fun(scenario.When[i])
+end
+
+for i, v in ipairs(scenario.Then) do
+    _Then:fun(scenario.Then[i])
+end
 
 print("Test OK!")
